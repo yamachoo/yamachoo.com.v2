2078fe42ff2d400a5d08d3ca4f5f6128ac74b186
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-08-28 11:15:47 +0900

    add: remark-code-titles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import remarkCodeTitles from 'remark-code-titles'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [remarkCodeTitles],
+    extendDefaultPlugins: true
+  }
 })
