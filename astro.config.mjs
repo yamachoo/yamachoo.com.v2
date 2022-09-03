bbb2e67127c8c053bdfe6d95737ebfe02440ff72
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-09-04 00:07:53 +0900

    add: astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
+import compress from 'astro-compress'
 import remarkCodeTitles from 'remark-code-titles'
 import remarkExternalLinks from 'remark-external-links'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yamachoo.com/',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), compress()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark-dimmed'
