b87e725581652f8746c1c34fcc3a61db84c9be65
yamachoo <51016095+yamachoo@users.noreply.github.com>
2022-12-04 13:53:06 +0900

    fix: extra spaces

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,5 +2,21 @@ module.exports = {
   semi: false,
   singleQuote: true,
   tabWidth: 2,
-  trailingComma: 'none'
+  trailingComma: 'none',
+  // TODO: Prettierをv3に上げたら削除する
+  // See: https://github.com/tats-u/prettier-plugin-md-nocjsp#target-versions
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        parser: 'markdown-nocjsp'
+      }
+    },
+    {
+      files: '*.mdx',
+      options: {
+        parser: 'mdx-nocjsp'
+      }
+    }
+  ]
 }
